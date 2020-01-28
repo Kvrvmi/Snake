68497e4869aef076d09ddcac8eec50c7120dd210
Kvrvmi <56537142+Kvrvmi@users.noreply.github.com>
2020-01-28 19:47:02 +0100

    Add files via upload

---
+++ 111.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+bool gameOver;
+const int width = 40;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+void Setup()
+{
+	gameOver = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand() % width;
+	fruitY = rand() % height;
+	score = 0;
+}
+void Draw()
+{
+	system("cls");
+	for (int i = 0; i < width; i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+
+		{
+			if (j == 0)
+				cout << "#";
+			if (i == y && j == x)
+				cout << "0";
+			else if (i == fruitY && j == fruitX)
+				cout << "F";
+			else
+				cout << " ";
+			if (j == width - 1)
+				cout << "#";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < width; i++)
+		cout << "#";
+	cout << endl;
+}	
+void Input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+		case 'd':
+			dir = RIGHT;
+		case 'w':
+			dir = UP;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	  }
+}
+void Logic()
+{
+	switch (dir)
+	{ 
+case LEFT:
+	x--;
+	break;
+case RIGHT:
+	x++;
+	break;
+case UP:
+	y--;
+	break;
+case DOWN:
+	y++;
+	break;
+default:
+	break;
+	}
+}
+int main()
+{
+	Setup();
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logic();
+		return 0;
+
+	}
+
+
+}
